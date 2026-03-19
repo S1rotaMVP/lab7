863d487371a00cb6798698a0f6a9e9e3057937e8
Любомир Онисько <lubomyronysko@gmail.com>
2026-03-19 23:57:33 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace lab8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Action> actions = new List<Action>();
+
+            for (int i = 1; i <= 5; i++)
+            {
+                actions.Add(() => Console.WriteLine(i));
+            }
+            foreach (var action in actions)
+            {
+                action();
+            }
+        }
+    }
+}
